9b3f2e3b8e490345b97ee1f8940feb0b9306423c
Jason Tarka <jasonmtarka@gmail.com>
2021-06-09 22:30:41 +0900

    Refactoring Update
    - Separated tkinter/GUI elements into a separate password_app file

---
+++ password_app.py
@@ -0,0 +1,131 @@
+import pyperclip
+import os
+import tkinter as tk
+
+from password_generator import Password
+
+# Class which manages UI
+class MainApplication:
+
+    def __init__(self, root) -> None:
+        self.root = root
+        self._initUI()
+
+    def _password_delivery(self) -> None:
+        # Bus between Password class and Tkinter UI
+        password = Password(lowercase=self.lc_var.get(), uppercase=self.uc_var.get(), nums=self.num_var.get(), syms=self.sym_var.get(),
+                            min_nums=self.min_num_clicked.get(), min_syms=self.min_sym_clicked.get(), pass_len=self.len_clicked.get())
+        self.e.delete(0, tk.END)
+        self.e.insert(0, password)
+
+    def _initUI(self) -> None:
+        greeting = tk.Label(self.root, text="Generate random passwords!", width=40, borderwidth=0, pady=10)
+        greeting.pack(pady=5)
+
+        e = tk.Entry(self.root, width=35, borderwidth=2)
+        e.pack()
+        self.e = e
+
+        # Creates six frames for UI layout
+        frames = {}
+        for i in range(1, 7):
+            frames[i] = PackedFrame(self.root)
+
+        lc_var = tk.IntVar()
+        lc_button = tk.Checkbutton(frames[1], text="Lowercase", variable=lc_var)
+        lc_button.pack(side="left", pady=2, padx=3)
+        lc_button.select()
+        self.lc_var = lc_var
+
+        uc_var = tk.IntVar()
+        uc_button = tk.Checkbutton(frames[1], text="Uppercase", variable=uc_var)
+        uc_button.pack(side="right", pady=2, padx=3)
+        uc_button.select()
+        self.uc_var = uc_var
+
+        num_var = tk.IntVar()
+        num_button = tk.Checkbutton(frames[2], text="Numbers", variable=num_var)
+        num_button.pack(side="left", pady=2, padx=1)
+        num_button.select()
+        self.num_var = num_var
+
+        sym_var = tk.IntVar()
+        sym_button = tk.Checkbutton(frames[2], text="Symbols", variable=sym_var)
+        sym_button.pack(side="right", pady=2, padx=11)
+        self.sym_var = sym_var
+
+        min_num_label = tk.Label(frames[3], text="Select minimum amount of numbers:")
+        min_num_label.pack(side="left")
+        min_num_options = [str(i) for i in range(0, 5)]
+        min_num_clicked = tk.StringVar()
+        min_num_clicked.set(min_num_options[1])
+        min_num_bar = tk.OptionMenu(frames[3], min_num_clicked, *min_num_options)
+        min_num_bar.configure(activebackground="#d4d4ff")
+        min_num_bar.pack(side="left", pady=5)
+        self.min_num_clicked = min_num_clicked
+
+        min_sym_label = tk.Label(frames[4], text="Select minimum amount of symbols:")
+        min_sym_label.pack(side="left")
+        min_sym_options = [str(i) for i in range(0, 5)]
+        min_sym_clicked = tk.StringVar()
+        min_sym_clicked.set(min_sym_options[1])
+        min_sym_bar = tk.OptionMenu(frames[4], min_sym_clicked, *min_sym_options)
+        min_sym_bar.configure(activebackground="#d4d4ff")
+        min_sym_bar.pack(side="right", pady=5)
+        self.min_sym_clicked = min_sym_clicked
+
+        len_label = tk.Label(frames[5], text="Select length of password (between 8-32 characters):")
+        len_label.pack(side="left")
+        len_options = [str(i) for i in range(8, 33)]
+        len_clicked = tk.StringVar()
+        len_clicked.set(len_options[0])
+        len_bar = tk.OptionMenu(frames[5], len_clicked, *len_options)
+        len_bar.configure(activebackground="#c9c9f2")
+        len_bar.pack(side="right", pady=5)
+        self.len_clicked = len_clicked
+
+        generate_button = HoverButton(frames[6], text="Generate", padx=25, pady=4, borderwidth=3, command=self._password_delivery)
+        generate_button.configure(activebackground="#c9c9f2")
+        generate_button.pack(padx=5, pady=5, side="left")
+
+        copy_button = HoverButton(frames[6], text="Copy Password", padx=15, pady=4, borderwidth=3, command=lambda: pyperclip.copy(self.e.get()))
+        copy_button.configure(activebackground="#d4d4ff")
+        copy_button.pack(padx=5, pady=5, side="right")
+
+
+# Class for changing button color when highlighting with cursor
+class HoverButton(tk.Button):
+
+    def __init__(self, master, **kwargs) -> None:
+        tk.Button.__init__(self, master=master, **kwargs)
+        self.defaultBackground = self["background"]
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, z) -> None:
+        self["background"] = self["activebackground"]
+
+    def on_leave(self, z) -> None:
+        self["background"] = self.defaultBackground
+
+# Class which automatically packs frames on instantiation
+class PackedFrame(tk.Frame):
+
+    def __init__(self, master, **kwargs) -> None:
+        tk.Frame.__init__(self, master=master, **kwargs)
+        self.pack()
+
+
+def main() -> None:
+    # Function which sets up the interactive window
+    root = tk.Tk()
+    MainApplication(root)
+    root.title("Password Generator")
+    root.iconbitmap(os.environ.get("PASSWORD_GEN_KEY_ICON"))
+    root.geometry('400x310')
+    root.resizable(height=False, width=False)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
